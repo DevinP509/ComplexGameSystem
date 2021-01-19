6c9a83c44352762bd974a0d718d0cf1998ab9d43
DevinP509 <54288889+DevinP509@users.noreply.github.com>
2021-01-19 09:44:43 -0600

    Created Git
    The Blueprints have changed a bit from the start

---
+++ Source/ComplexGameTest/BulletKinematics.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "BulletKinematics.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMPLEXGAMETEST_API UBulletKinematics : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
