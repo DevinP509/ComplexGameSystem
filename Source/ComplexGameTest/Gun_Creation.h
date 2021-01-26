09dd1fa030decc76261ad8c96be065e47f8daacf
DevinP509 <54288889+DevinP509@users.noreply.github.com>
2021-01-26 15:18:46 -0600

    Added C++ Scripts

---
+++ Source/ComplexGameTest/Gun_Creation.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class COMPLEXGAMETEST_API Gun_Creation
+{
+public:
+	Gun_Creation();
+	~Gun_Creation();
+};
---
+++ Source/ComplexGameTest/Test.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ChildActorComponent.h"
+#include "Test.generated.h"
+
+
+UCLASS()
+class COMPLEXGAMETEST_API UTest : public UChildActorComponent
+{
+	GENERATED_BODY()
+	
+};
